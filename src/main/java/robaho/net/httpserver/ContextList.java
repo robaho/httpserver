73cde47504ba4894723847377b8d077bceddb8ba
robert engels <robaho@users.noreply.github.com>
2024-06-27 15:22:37 -0500

    stash exact context lookups

--- src/main/java/robaho/net/httpserver/ContextList.java
+++ src/main/java/robaho/net/httpserver/ContextList.java
@@ -26,11 +26,14 @@
 package robaho.net.httpserver;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 class ContextList {
 
     private final List<HttpContextImpl> list = new CopyOnWriteArrayList<>();
+    private final Map<CacheKey,HttpContextImpl> cache = new ConcurrentHashMap<>();
+    private record CacheKey(String protocol,String path){}
 
     public synchronized void add(HttpContextImpl ctx) {
         assert ctx.getPath() != null;
@@ -53,15 +56,24 @@ class ContextList {
      * Context with longest prefix matches (currently case-sensitive)
      */
     HttpContextImpl findContext(String protocol, String path) {
-        return findContext(protocol, path, false);
+        CacheKey key = new CacheKey(protocol,path);
+        var ctx = cache.get(key);
+        if(ctx!=null) return ctx;
+        ctx = findContext(protocol, path, false);
+        // only cache exact matches, otherwise path parameters will 
+        // explode memory
+        if(ctx!=null && ctx.getPath().equals(path)) {
+            cache.put(key,ctx);
+        }
+        return ctx;
     }
 
     HttpContextImpl findContext(String protocol, String path, boolean exact) {
-        protocol = protocol.toLowerCase(Locale.ROOT);
+        String _protocol = protocol.toLowerCase(Locale.ROOT);
         String longest = "";
         HttpContextImpl lc = null;
         for (HttpContextImpl ctx : list) {
-            if (!ctx.getProtocol().equals(protocol)) {
+            if (!ctx.getProtocol().equals(_protocol)) {
                 continue;
             }
             String cpath = ctx.getPath();
