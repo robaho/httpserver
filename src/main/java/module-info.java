ff70ce6a94e730d2d3cd8e382601bbafefed3809
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-11-22 00:05:48 -0500

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module robaho.httpserver {
     exports robaho.net.httpserver.extras;
     exports robaho.net.httpserver.websockets;
 
+    requires java.logging;
+    requires java.net.http;
     requires jdk.httpserver;
-    
+
     provides com.sun.net.httpserver.spi.HttpServerProvider with robaho.net.httpserver.DefaultHttpServerProvider;
 }
