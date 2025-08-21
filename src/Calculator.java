850e83b894613715a74c2e401a2a1d72133bbc97
Mehedi-86 <mehedihasan@mehedis-MacBook-Air.local>
2025-08-21 19:01:38 +0600

    Added Calculator.java with basic arithmetic methods

---
+++ src/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    // Addition
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Division
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+}
