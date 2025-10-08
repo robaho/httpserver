7896dac6c8848f8099d135845c3532fad9a483d6
robert engels <robaho@users.noreply.github.com>
2025-10-08 17:29:47 -0500

    test 100 Continue to Expect header

--- src/test/java/InputRead100Test.java
+++ src/test/java/InputRead100Test.java
@@ -1,26 +1,3 @@
-/*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 /**
  * @test id=default
  * @bug 8349670
@@ -41,34 +18,17 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 import static java.nio.charset.StandardCharsets.*;
 
 public class InputRead100Test {
     private static final String someContext = "/context";
-
-    static class ServerThreadFactory implements ThreadFactory {
-        static final AtomicLong tokens = new AtomicLong();
-
-        @Override
-        public Thread newThread(Runnable r) {
-            var thread = new Thread(r, "Server-" + tokens.incrementAndGet());
-            thread.setDaemon(true);
-            return thread;
-        }
-    }
 
     static {
         Logger.getLogger("").setLevel(Level.ALL);
@@ -76,12 +36,10 @@ public class InputRead100Test {
     }
 
     @Test
-    public void testContinue() throws Exception {
+    public static void testContinue() throws Exception {
         System.out.println("testContinue()");
         InetAddress loopback = InetAddress.getLoopbackAddress();
         HttpServer server = HttpServer.create(new InetSocketAddress(loopback, 0), 0);
-        ExecutorService executor = Executors.newCachedThreadPool(new ServerThreadFactory());
-        server.setExecutor(executor);
         try {
             server.createContext(
                 someContext,
@@ -89,18 +47,10 @@ public class InputRead100Test {
                     System.err.println("Handling request: " + msg.getRequestURI());
                     byte[] reply = "Here is my reply!".getBytes(UTF_8);
                     try {
-                        try {
-                            msg.getResponseHeaders().add("Header", "BeforeContinue");
-                            msg.sendResponseHeaders(100, -1);
-                            BufferedReader r = new BufferedReader(new InputStreamReader(msg.getRequestBody()));
-                            r.read();
-                            msg.sendResponseHeaders(200, reply.length);
-                            msg.getResponseBody().write(reply);
-                            msg.getResponseBody().close();
-                            Thread.sleep(50);
-                        } catch (IOException | InterruptedException ie) {
-                            ie.printStackTrace();
-                        }
+                        msg.getRequestBody().readAllBytes();
+                        msg.sendResponseHeaders(200, reply.length);
+                        msg.getResponseBody().write(reply);
+                        msg.getResponseBody().close();
                     } finally {
                         System.err.println("Request handled: " + msg.getRequestURI());
                     }
@@ -108,10 +58,9 @@ public class InputRead100Test {
             server.start();
             System.out.println("Server started at port " + server.getAddress().getPort());
 
-            runRawSocketHttpClient(loopback, server.getAddress().getPort(), 64 * 1024 + 16);
+            runRawSocketHttpClient(loopback, server.getAddress().getPort(), 0);
         } finally {
             System.out.println("shutting server down");
-            executor.shutdown();
             server.stop(0);
         }
         System.out.println("Server finished.");
@@ -122,6 +71,9 @@ public class InputRead100Test {
         Socket socket = null;
         PrintWriter writer = null;
         BufferedReader reader = null;
+
+        boolean foundContinue = false;
+
         final String CRLF = "\r\n";
         try {
             socket = new Socket(address, port);
@@ -141,19 +93,32 @@ public class InputRead100Test {
             // there's more into the request.
             writer.flush();
             System.out.println("Client wrote request to socket: " + socket);
-
-            writer.print(body);
-            writer.flush();
-            System.out.println("Client wrote body to socket: " + socket);
-
+            System.out.println("Client read 100 Continue response from server and headers");
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println("Client start reading from server:");
             String line = reader.readLine();
             for (; line != null; line = reader.readLine()) {
                 if (line.isEmpty()) {
                     break;
                 }
-                System.out.println("\"" + line + "\"");
+                System.out.println("interim response \"" + line + "\"");
+                if (line.startsWith("HTTP/1.1 100")) {
+                    foundContinue = true;
+                }
+            }
+            if (!foundContinue) {
+                throw new IOException("Did not receive 100 continue from server");
+            }
+            writer.print(body);
+            writer.flush();
+            System.out.println("Client wrote body to socket: " + socket);
+
+            System.out.println("Client start reading from server:");
+            line = reader.readLine();
+            for (; line != null; line = reader.readLine()) {
+                if (line.isEmpty()) {
+                    break;
+                }
+                System.out.println("final response \"" + line + "\"");
             }
             System.out.println("Client finished reading from server");
         } finally {
